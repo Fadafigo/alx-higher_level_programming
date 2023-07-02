c2091218260e87d2ed5fd8ddd7d635e8fc07f42e
Julius <fadafigo100@gmail.com>
2023-07-02 00:02:30 +0000

    python-network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,10 +5,10 @@ Module to find the hightest number in an array
 
 
 def find_peak(list_of_integers):
-        """
-            Method to find hightest number in array
-                """
-                    if bool(list_of_integers) is False:
-                                return None
-                                list_of_integers.sort()
-                                    return list_of_integers[-1]
+    """
+    Method to find hightest number in array
+    """
+    if bool(list_of_integers) is False:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
