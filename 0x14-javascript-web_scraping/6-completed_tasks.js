d76e8dffacd86e8aad311e7e68e48f62a6ab7c19
julius <fadafigo100@gmail.com>
2023-08-02 15:41:54 +0000

    Javascript Web Scrapping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,4 @@
-#!/usr/bin/node
-
-const request = require('request');
-const url = process.argv[2];
-
-request.get(url, { json: true }, (error, response, body) => {
+const request = require('request');const url = process.argv[2];request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
